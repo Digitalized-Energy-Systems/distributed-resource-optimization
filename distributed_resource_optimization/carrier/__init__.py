1a3c04795c448d93fb542f3f95012681238bd1e8
Rico Schrage <rico.schrage@gmail.com>
2026-06-22 03:43:49 +0200

    Renaming mango.py to mango_carrier.py

--- distributed_resource_optimization/carrier/__init__.py
+++ distributed_resource_optimization/carrier/__init__.py
@@ -16,3 +16,10 @@ __all__ = [
     "start_distributed_optimization",
     "start_coordinated_optimization",
 ]
+
+import sys as _sys
+from importlib import import_module as _import_module
+from importlib.util import find_spec as _find_spec
+
+if _find_spec("mango") is not None:
+    _sys.modules[f"{__name__}.mango"] = _import_module(f"{__name__}.mango_carrier")
